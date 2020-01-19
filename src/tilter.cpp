f46755c6df97fb816429f6c62b89b248a9517d55
will1008m <will.hackemer@gmail.com>
2020-01-19 12:29:49 -0600

    Separated async tilter and tilter driver control

--- src/tilter.cpp
+++ src/tilter.cpp
@@ -3,6 +3,7 @@
 Motor tilter1(TILTER, MOTOR_GEARSET_18, 0, MOTOR_ENCODER_DEGREES);
 unsigned int tilterTarget = 0;
 int tiltSpeed = 127;
+double tiltKp = 3.5;
 
 motor_brake_mode_e_t tiltMode = MOTOR_BRAKE_HOLD;
 
@@ -24,28 +25,28 @@ void asyncTilterTo(int tiltPoint){
   tilterTarget = tiltPoint;
 }
 
+int tilterPos()
+{
+  return tilter1.get_position();
+}
 
 /**************************************************/
 //task control
 void tilterTask(void* parameter){
   int prevError = 0;
 
-  while(1){
+  while(competition::is_autonomous() == true){
     delay(20);
 
     int sp = tilterTarget;
-
-    double kp = 3.5;
-    double kd = 3.0;
 
     //read sensors
     int sv = tilter1.get_position();
 
     //speed
     int error = sp-sv;
-    int derivative = error - prevError;
     prevError = error;
-    int speed = error*kp + derivative*kd;
+    int speed = error*tiltKp;
     //printf("%f @ %d\n", tilter1.get_position(), speed);
 
     if(speed > tiltSpeed)
@@ -54,42 +55,43 @@ void tilterTask(void* parameter){
       speed = -tiltSpeed;
 
     //set motors
-    tilter(speed);
+
+    if(tilter1.get_position< (1920/2))
+      tilter(tiltSpeed);
+    else
+      tilter(speed);
+
   }
 }
 
-int tilterPos()
-{
-  return tilter1.get_position();
-}
 
 void tilterOp(){
   //printf("%f\n", tilter1.get_position());
   tilter1.set_brake_mode(tiltMode);
-  //starts 0, ends 1920
   if(master.get_digital(DIGITAL_X))
     {
-      if(tilter1.get_position() < (1920/2))
-        tilter(127);
+      if(tilterPos() < (1920/2))
+        tilter(100); int prevError = 0;
       else
-        tilterTarget = 1920;
+      {
+        int sp = 1650;
+
+        //read sensors
+        int sv = tilter1.get_position();
+
+        //speed
+        int error = sp-sv;
+        prevError = error;
+        int speed = error*tiltKp;
+        //printf("%f @ %d\n", tilter1.get_position(), speed);
+
+        if(speed > tiltSpeed)
+          speed = tiltSpeed;
+        tilter(40);
+      }
     }
-    //tiltTarget = tiltTarget+7;
   else if(master.get_digital(DIGITAL_B))
     tilter(-127);
-    //tiltTarget = tiltTarget-7;
-    else
-    tilter(0);
-  /*if(master.get_digital(DIGITAL_B))
-  {
-    tiltMode = MOTOR_BRAKE_COAST;
-    //tilt1.set_brake_mode(tiltMode);
-    //tilt2.set_brake_mode(tiltMode);
-  }
   else
-  {
-    tiltMode = MOTOR_BRAKE_HOLD;
-    //tilt1.set_brake_mode(tiltMode);
-    //tilt2.set_brake_mode(tiltMode);
-  }*/
+    tilter(0);
 }
